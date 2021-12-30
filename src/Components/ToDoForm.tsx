5d5b52087faf1bb7b4dc3831eaea59651ba9702d
Mark-Vale <markpvale@gmail.com>
2021-12-29 16:27:52 -0800

    ts-todo base

---
+++ src/Components/ToDoForm.tsx
@@ -0,0 +1,45 @@
+import * as React from 'react'
+import {TodoInterface, TodoFormInterface} from '../interface'
+
+let id = '0';
+
+const ToDoForm = (props: TodoFormInterface) => {
+  const inputRef = React.useRef<HTMLInputElement>(null)
+  const [values, setValues] = React.useState('')
+
+  function handleInputChange(event:
+    React.ChangeEvent<HTMLInputElement>) {
+      setValues(event.target.value)
+    }
+    function handleInputEnter(event:
+      React.KeyboardEvent) {
+        //check for enter key
+        if (event.key === 'Enter') {
+          //prepare new todo object
+          const newTodo: TodoInterface = {
+            id: id + '1',
+            name: values,
+            isCompleted: false
+          }
+          //Create new todo item
+          props.handleTodoCreate(newTodo)
+          //Reset the input field
+          if (inputRef && inputRef.current) {
+            inputRef.current.value = ''
+          }
+        }
+      }
+      return (
+        <div className='todo-form'>
+          <input
+            ref={inputRef}
+            type='text'
+            placeholder='Enter New Todo'
+            onChange={event => handleInputChange(event)}
+            onKeyPress={event => handleInputEnter(event)}
+          />
+        </div>
+      )
+}
+
+export default ToDoForm
